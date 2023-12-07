0ab8a2043df641537e7887545ab9f043247a8e25
LEv145 <you@example.com>
2023-12-08 00:57:20 +0200

    Add decrypt functions

--- caesar_cipher.cpp
+++ caesar_cipher.cpp
@@ -11,7 +11,7 @@
 namespace caesar_cipher {
     const std::string DICTIONARY = " .?!,;:-ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    std::string crypt(const std::string& text, unsigned key) {
+    std::string crypt(const std::string& text, long key) {
         std::string result;
 
         for (auto i: text) {
@@ -22,6 +22,10 @@ namespace caesar_cipher {
         return result;
     }
 
+    std::string decrypt(const std::string& text, long key) {
+        return crypt(text, -key);
+    }
+
     std::string crypt_with_random_insertion(const std::string& text, unsigned key, unsigned seed) {
         std::string result;
 
@@ -30,12 +34,37 @@ namespace caesar_cipher {
         generator.seed(seed);
         std::uniform_int_distribution<> distribution(0, DICTIONARY.length() - 1);
 
+        for (int i = 0; i < text.length();) {
+            auto random_value = distribution(generator);
+
+            if (random_value % 2 == 0) {
+                result += DICTIONARY[random_value];
+            } else {
+                auto new_index = (DICTIONARY.find(text[i]) + key) % DICTIONARY.length();
+                result += DICTIONARY[new_index];
+                i++;
+            }
+        }
+
+        return result;
+    }
+
+    std::string decrypt_with_random_insertion(const std::string& text, unsigned key, unsigned seed) {
+        std::string result;
+
+        std::random_device random_device;
+        std::mt19937 generator(random_device());
+        generator.seed(seed);
+        std::uniform_int_distribution<> distribution(0, DICTIONARY.length() - 1);
+
         for (auto i: text) {
-            if (distribution(generator) % 2 == 0) {
-                result += DICTIONARY[distribution(generator)];
+            auto random_value = distribution(generator);
+
+            if (random_value % 2 == 0) {
+                continue;
             }
 
-            auto new_index = (DICTIONARY.find(i) + key) % DICTIONARY.length();
+            auto new_index = (DICTIONARY.find(i) - key) % DICTIONARY.length();
             result += DICTIONARY[new_index];
         }
 
--- caesar_cipher.h
+++ caesar_cipher.h
@@ -2,6 +2,8 @@
 
 
 namespace caesar_cipher {
-    std::string crypt(const std::string& text, unsigned key);
+    std::string crypt(const std::string& text, long key);
+    std::string decrypt(const std::string& text, long key);
     std::string crypt_with_random_insertion(const std::string& text, unsigned key, unsigned seed);
+    std::string decrypt_with_random_insertion(const std::string& text, unsigned key, unsigned seed);
 }
